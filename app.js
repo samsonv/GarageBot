bfe71a8412377520c271ab64b72cb1d798c9b2ff
Samson Valland <samsonvalland@gmail.com>
2016-02-01 22:33:12 +0100

    socket.io-client in place!

--- app.js
+++ app.js
@@ -1,41 +1,23 @@
 var express = require('express');
 var execute = require('./execute');
-var got = require('got');
-var gpio = require('gpio');
-var five = require("johnny-five");
-var board = new five.Board();
-var app = express();
+var io = require('socket.io-client');
 
-const home = 'localhost:1337/phonehome';
-const phoneinterval = 3000;
+var client = io.connect("http://localhost:3000");
 
-app.get('/', function(req, res){
-    res.send('hello world');
+client.on('connect',function() {
+    console.log("connected!");
+    client.emit("pi","hello!");
 });
 
-app.get('/blink/:seconds', function(req, res){
-    var seconds = parseInt(req.params.seconds)*1000 || 2000;
-    res.send('should blink for ' + seconds + ' milliseconds');
-    var led = new five.Led(13);
-    led.on();
-    setTimeout(function(){
-        led.off()
-    }, seconds); 
+client.on('command', function(command){
+    console.log('Server tells me to: ', command);
+    
+    switch (command) {
+        case "status":
+            client.emit("pi", "I am ok");
+            break;
+        default:
+            client.emit("pi", "I don't understand");
+            break;
+    }
 })
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
-var phoneHome = function(){
-    console.log('phoning home..')
-    got(home)
-    .then(response => {
-        console.log(response.body);
-    })
-    .catch(error => {
-        console.log(error.response.body);
-    });
-}
-
-execute(phoneHome).immediately().regularily(phoneinterval);
