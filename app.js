3b98b2bba99b8a624c058681d1ca38e973599ba1
Samson Valland <samsonvalland@gmail.com>
2016-02-04 22:36:41 +0100

    Working

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
 var express = require('express');
-var execute = require('./execute');
 var io = require('socket.io-client');
+var Gpio = require('onoff').Gpio;
 
-var client = io.connect("localhost:3000");
+var site = process.argv[2] || 'localhost:3000';
+var client = io.connect(site);
 
 client.on('connect',function() {
-    console.log("connected!");
-    client.emit("pi","hello!");
+    console.log('connected!');
+    client.emit('pi','hello!');
 });
+
+var blink = function(time){
+    var led  = new Gpio(4, 'out');
+    led.writeSync(1);
+    setTimeout(function() {
+        led.writeSync(0);
+        led.unexport();
+    }, time);
+}
 
 client.on('command', function(command){
     console.log('Server tells me to: ', command);
-    
+    command = command.toLowerCase();
+        
     switch (command) {
-        case "status":
-            client.emit("pi", "I am ok");
+        case 'status':
+            client.emit('pi', 'I am ok');
+            break;
+        case 'blink':
+            client.emit('pi', 'ok, i am blink now');
+            blink(1000);
             break;
         default:
-            client.emit("pi", "Someone told me: " + command);
+            client.emit('pi', 'Someone told me: ' + command);
             break;
     }
 })
+
+console.log('trying to connect to ' + site);
---
+++ readme.md
@@ -0,0 +1,6 @@
+# What is this?
+A very simple Node app for Raspberry Pi that talks with the web as a socket.io-client and talks with the world using the onoff GPIO npm package. 
+
+# Usage
+1. git clone git://github.com/samsonv/GarageBot.git
+2. run 'npm install' and then run 'node app.js http://my-socketio-server.com' 
