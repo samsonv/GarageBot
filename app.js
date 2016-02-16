da42dff9ab042df7a878db6cf7c4da6c4c22e19c
Samson Valland <samsonvalland@gmail.com>
2016-02-16 20:27:59 +0100

    Introducin johnny-five again

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var io = require('socket.io-client');
 var Gpio = require('onoff').Gpio;
+var five = require("johnny-five");
+var board = new five.Board();
 
 var site = process.argv[2] || 'localhost:3000';
 var client = io.connect(site);
@@ -19,6 +21,15 @@ var blink = function(time){
     }, time);
 }
 
+var blinkWithArduino = function(seconds){
+    console.log('should blink for ' + seconds + ' milliseconds');
+    var led = new five.Led(13);
+    led.on();
+    setTimeout(function(){
+        led.off()
+    }, seconds); 
+}
+
 client.on('command', function(command){
     console.log('Server tells me to: ', command);
     command = command.toLowerCase();
@@ -27,9 +38,14 @@ client.on('command', function(command){
         case 'status':
             client.emit('pi', 'I am ok');
             break;
-        case 'blink':
-            client.emit('pi', 'ok, i am blink now');
+        case 'open':
+            client.emit('pi', 'Ok, opening with gpio 4 now');
             blink(1000);
+            break;
+            break;
+        case 'blink':
+            client.emit('pi', 'Ok, opening trough arduino now');
+            blinkWithArduino(5000);
             break;
         default:
             client.emit('pi', 'Someone told me: ' + command);
