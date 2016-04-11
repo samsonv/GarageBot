03ee748bd57a00cc597d21fac26e66d0cd23bee2
Samson Valland <samsonvalland@gmail.com>
2016-04-11 21:46:12 +0200

    Added sonic distance meassurer!

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 var express = require('express');
 var io = require('socket.io-client');
 var Gpio = require('onoff').Gpio;
-var five = require("johnny-five");
-var board = new five.Board({ repl: false });
+var garageInterval;
+var portIsRunning = false;
+var usonic = require('r-pi-usonic');
 
-var site = process.argv[2] || 'localhost:3000';
-var client = io.connect(site);
-
-client.on('connect',function() {
-    console.log('connected!');
-    client.emit('pi','hello!');
-});
-
-var blink = function(time){
-    var led  = new Gpio(4, 'out');
+var blink = function(time) {
+    var led = new Gpio(23, 'out');
     led.writeSync(1);
     setTimeout(function() {
         led.writeSync(0);
@@ -21,37 +14,54 @@ var blink = function(time){
     }, time);
 }
 
-var blinkWithArduino = function(seconds){
-    console.log('should blink for ' + seconds + ' milliseconds');
-    var led = new five.Led(12);
-    led.on();
-    setTimeout(function(){
-        led.off()
-    }, seconds); 
-}
+var handleCommand = function(command) {
+    command = command.toLowerCase().split(' ');
 
-client.on('command', function(command){
-    console.log('Server tells me to: ', command);
-    command = command.toLowerCase();
-        
-    switch (command) {
+    switch (command[0]) {
         case 'status':
-            client.emit('pi', 'I am ok');
-            break;
-        case 'open':
-            client.emit('pi', 'Ok, opening with gpio 4 now');
-            blink(1000);
-            break;
+            client.emit('pi', [
+                'Tar ein is i vårsola',
+                'Tek meg eit stev',
+                'Jobbar på i garasjen',
+                'Ventar på å lukke eller opne porten',
+                'Har det Ok',
+                'Tek det kuli',
+                'Alltid moro i garasjen',
+                'Anbefalar ein sykkeltur!',
+                'Litt kaldt her idag,',
+                'No er våren i kjømda!'][Math.floor(Math.random() * 10)]);
             break;
         case 'blink':
-            client.emit('pi', 'Ok, opening trough arduino now');
-            blinkWithArduino(1000);
-            break;
+            var dur = command[1] || 1000;
+            openOrClose(dur);
+            client.emit('pi', 'Ok, opnar i ' + dur + ' millisekund');
             break;
         default:
-            client.emit('pi', 'Someone told me: ' + command);
+            client.emit('pi', 'Eg fortstod ikkje meldinga di: "' + command + '". Prøv <status> eller <blink>');
             break;
     }
-})
+}
+
+usonic.init(function(error) {
+    if (error) {
+        console.log('fail!', error);
+    } else {
+        var sensor = usonic.createSensor(21, 17, 450);
+        var site = process.argv[2] || 'localhost:3000';
+        var client = io.connect(site);
+
+        client.on('connect', function() {
+            console.log('connected!');
+            client.emit('pi', 'hello!');
+
+            setInterval(function() {
+                var distance = sensor();
+                client.emit('distance-message', distance);
+            }, 1000);
+        });
+
+        client.on('command', handleCommand)
+    }
+});
 
 console.log('trying to connect to ' + site);
