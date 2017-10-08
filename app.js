df0badb990306d5d7909c4ddefe1d56eba3532fc
Samson Valland <samsonvalland@gmail.com>
2017-10-08 22:53:07 +0200

    remove socketio

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io-client');
 var Gpio = require('onoff').Gpio;
 var garageInterval;
 var portIsRunning = false;
 var usonic = require('r-pi-usonic');
 
 var site = process.argv[2] || 'localhost:3000';
-var client = io.connect(site);
 var isBusyBlinking = false;
 var turnOff;
 var reportDistance;
@@ -32,33 +30,10 @@ var blink = function (time) {
     }, time);
 }
 
-var handleCommand = function (command) {
-    command = command.toLowerCase().split(' ');
-
-    switch (command[0]) {
-        case 'status':
-            client.emit('pi', [
-                'Tar ein is i vårsola',
-                'Tek meg eit stev',
-                'Jobbar på i garasjen',
-                'Ventar på å lukke eller opne porten',
-                'Har det Ok',
-                'Tek det kuli',
-                'Alltid moro i garasjen',
-                'Anbefalar ein sykkeltur!',
-                'Litt kaldt her idag,',
-                'No er våren i kjømda!'][Math.floor(Math.random() * 10)]);
-            break;
-        case 'blink':
-            var dur = command[1] || 1000;
-            if (!isBusyBlinking) {
-                blink(dur);
-                client.emit('pi', 'Ok, opnar i ' + dur + ' millisekund');
-            }
-            break;
-        default:
-            client.emit('pi', 'Eg fortstod ikkje meldinga di: "' + command + '". Prøv <status> eller <blink>');
-            break;
+var handleCommand = function () {
+    var dur = 1000;
+    if (!isBusyBlinking) {
+        blink(dur);
     }
 }
 
@@ -69,11 +44,11 @@ app.get('/', function (req, res) {
 });
 
 app.get('/open', function (req, res) {
-    handleCommand('blink');
+    handleCommand();
     res.send('ok, ')
 });
 
-app.get('/distance', function(req, res){
+app.get('/distance', function (req, res) {
     res.send(self.distance);
 })
 
@@ -83,26 +58,18 @@ usonic.init(function (error) {
     } else {
         var sensor = usonic.createSensor(21, 17, 450);
 
-        client.on('connect', function () {
-            console.log('connected!');
-            client.emit('pi', 'Hei.');
+        if (reportDistance) {
+            clearInterval(reportDistance);
+        }
 
-            if (reportDistance) {
-                clearInterval(reportDistance);
-            }
+        reportDistance = setInterval(function () {
+            self.distance = sensor();
+        }, 5000);
 
-            reportDistance = setInterval(function () {
-                self.distance = sensor();
-                client.emit('distance-message', self.distance);
-            }, 5000);
+        http.listen(80, function () {
+            console.log('listening on *:' + 80);
         });
-
-        client.on('command', handleCommand)
     }
 });
 
 console.log('trying to connect to ' + site);
-
-http.listen(80, function () {
-    console.log('listening on *:' + 80);
-});
